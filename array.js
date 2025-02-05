6ee709784e64f21f9f4164aa367dfbfce1a5be3c
F223648 <155681864+F223648@users.noreply.github.com>
2025-02-05 18:48:15 +0500

    Lecture practices

---
+++ array.js
@@ -0,0 +1,28 @@
+let fruits = new Array("apple", "banana", "orange");
+//  fruits.splice(1,1,"ibraheem");
+// console.log(fruits.slice(0,2).indexOf("appricote"));
+
+// fruits.forEach((value)=>{
+//     console.log(value);
+
+// })
+
+//  let htmlListItem = fruits.map(val=> "<li>"+val+"</li>");
+
+let numbers = new Array(1,2,3,4,5);
+
+// let out = numbers.reduce(((res,curr)=>{
+//     res * curr;
+// }),1);
+
+// let outputa = fruits.reduce(((acc,val)=>acc+"<li>"+val+"</li>"),"<ul>");
+// outputa += "</ul>";
+// console.log(outputa);
+
+// let max = numbers.reduce((()));
+
+
+let arr = [[1],[2]];
+
+// let a = arr.reduce(((acc,val)=>acc.concat(val)));   concat a 2d array into single d 
+
